af465896457ac9db71a712f48edba3eb5b922020
papew28 <cissepapewaly@gmail.com>
2024-06-19 22:24:14 +0100

    update

--- jobs/main.py
+++ jobs/main.py
@@ -1,7 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.avro.functions import from_avro
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, explode
+from cassandra.cluster import Cluster
 import json
+
+
 
 avro_schema=json.dumps({
     "namespace":"kafka.broker.producer",
@@ -71,14 +74,38 @@ def create_spark_session():
                         .config("spark.jars.packages", 
                                 "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,"
                                 "org.apache.hadoop:hadoop-client:3.4.0,"
-                                "org.apache.spark:spark-avro_2.12:3.5.1"
+                                "org.apache.spark:spark-avro_2.12:3.5.1,"
+                                "com.datastax.spark:spark-cassandra-connector_2.12:3.5.0"
                                 ) \
                         .getOrCreate()
     return spark
 
-def create_cassandra_keyspace():
+#configuration de cassandra
+def create_keyspace(session):
+    session.execute("CREATE KEYSPACE IF NOT EXISTS finhub_streaming \
+                     WITH REPLICATION= {'class': 'SimpleStrategy', 'replication_factor': 1};")
 
-    pass
+
+def create_table(session):
+    session.execute("CREATE TABLE IF NOT EXISTS finhub_streaming.trading_data \
+                     (symbol text, price float, timestamp timestamp, volume float, PRIMARY KEY (symbol, timestamp));")
+    
+
+def create_session():
+    cluster = Cluster(['localhost'])
+    session = cluster.connect()
+    return session
+
+def for_each_batch_function(df, epoch_id):
+    
+    print(f"writing data to cassandra for  {epoch_id}")
+    df.write \
+       .format("org.apache.spark.sql.cassandra") \
+       .options(table="trading_data", keyspace="finhub_streaming") \
+       .mode("append") \
+       .save()
+
+     
 
 def main():
     spark = create_spark_session()
@@ -92,13 +119,33 @@ def main():
     kafka_stream_data = kafka_stream.selectExpr("CAST(value AS BINARY)")
     kafka_deserialized = kafka_stream_data.select(from_avro(col("value"), avro_schema).alias("data"))
     kafka_final_data = kafka_deserialized.select("data.*")
-    query = kafka_final_data \
-            .writeStream \
-            .outputMode("append") \
-            .format("console") \
-            .start()
+    trading_data =kafka_final_data.select(explode(col("data")).alias("data"))\
+                                  .select("data.*")
+    
+
+    trading_data = trading_data \
+    .withColumnRenamed("c", "trading condition") \
+    .withColumnRenamed("s", "symbol") \
+    .withColumnRenamed("p", "price") \
+    .withColumnRenamed("t", "timestamp") \
+    .withColumnRenamed("v", "volume")
+
+    transformed_data = trading_data.withColumn(
+    "timestamp",
+    (col("timestamp") / 1000).cast("timestamp")
+)
+        
+    cassandra_session = create_session()
+    create_keyspace(cassandra_session)
+    create_table(cassandra_session)
+
+    query = transformed_data.writeStream \
+        .foreachBatch(for_each_batch_function) \
+        .start()
 
     query.awaitTermination()
+
+    cassandra_session.shutdown()
 
 if __name__ == "__main__":
     main()
---
+++ streaming/streamapp.py
@@ -0,0 +1,60 @@
+from confluent_kafka import  SerializingProducer
+import websocket
+import os
+import sys
+import json
+
+sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.functions import validate_ticker,encode_avro
+from utils.constant import avro_schema,ticker,api_key
+
+
+producer_conf={
+    'bootstrap.servers': 'localhost:9092'
+}
+
+producer=SerializingProducer(producer_conf)
+
+topic_name='trading_data'
+
+def on_message(ws, message):
+    message=json.loads(message)
+    avro_mess=encode_avro(avro_schema,{
+                            "data":message["data"],
+                            "type":message["type"]
+                          } 
+                       )
+    producer.produce(topic_name,value=avro_mess)
+
+    producer.poll(0)
+    
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    #validate the ticker
+    #for tiker in ticker:
+        if validate_ticker(ticker):
+            #subsribe to the ticker
+            ws.send(f'{{"type":"subscribe","symbol":"{ticker}"}}')
+        else:
+            print(f"{ticker} is not a valid ticker")
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(f"wss://ws.finnhub.io?token={api_key}",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+
+
+
+
+
